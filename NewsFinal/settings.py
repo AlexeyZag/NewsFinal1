c9a8aee00aa32b4261ec5b478079a9d9d5aedfb2
AlexeyZag <alex8.8@mail.ru>
2021-04-11 22:03:03 +0300

    edit

--- NewsFinal/settings.py
+++ NewsFinal/settings.py
@@ -169,7 +169,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 SITE_ID = 1
 AUTH_USER_MODEL = 'users.CustomUser'
